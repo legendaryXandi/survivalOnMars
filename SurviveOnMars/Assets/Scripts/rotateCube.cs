de61a7ec3aaa3137242ee7f90d534ec97b95685e
legendaryXandi <45796104+legendaryXandi@users.noreply.github.com>
2019-11-02 15:29:41 +0100

    new scenes for testing and new assets

---
+++ SurviveOnMars/Assets/Scripts/rotateCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCube : MonoBehaviour
+{
+	public float spinForce;
+	private bool isSpinning = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(isSpinning){
+			transform.Rotate(0,spinForce * Time.deltaTime,0);
+		} else{
+			transform.Rotate(0,0,0);
+		}
+    }
+	
+	public void changeSpin(){
+		isSpinning = !isSpinning;
+	}
+}
