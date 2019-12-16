ba1cf98e0000b5e46297b9bd27d941780916618e
legendaryXandi <45796104+legendaryXandi@users.noreply.github.com>
2019-12-17 00:36:37 +0100

    major improvements

---
+++ OculusQuestSurvivalOnMars/Assets/Scripts/doorOpener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpener : MonoBehaviour
+{
+    public GameObject door;
+	private bool doorIsOpening;
+	private bool doorHasBeenOpened;
+	
+	public AudioSource audioData;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(!doorHasBeenOpened){
+			if(transform.position.y < 5.75f && !doorIsOpening){
+				doorIsOpening = true;
+				audioData.Play(2);
+			}
+			if(doorIsOpening){
+				door.transform.Translate(Vector3.up*Time.deltaTime*3);
+			}
+			if(door.transform.position.y > 12.45f){
+				doorHasBeenOpened = true;
+			}
+		}
+    }
+}
---
+++ OculusQuestSurvivalOnMars/Assets/Scripts/pickUpWalki.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpWalki : MonoBehaviour
+{
+	
+	private OVRGrabbable ovrGrabbable;
+	private OVRInput.Button buttonA;
+	public GameObject text;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ovrGrabbable.isGrabbed){
+			text.SetActive(true);
+		}
+    }
+}
