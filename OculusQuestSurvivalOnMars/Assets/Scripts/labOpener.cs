32dc8851aec85af8e706ad8843616b1c180bb875
legendaryXandi <45796104+legendaryXandi@users.noreply.github.com>
2019-12-28 22:48:56 +0100

    greenhouse plants analyzing

--- OculusQuestSurvivalOnMars/Assets/Scripts/labOpener.cs
+++ OculusQuestSurvivalOnMars/Assets/Scripts/labOpener.cs
@@ -5,24 +5,107 @@ using UnityEngine;
 public class labOpener : MonoBehaviour
 {
 	public GameObject door;
-	private bool isOpen;
-	private bool doorIsOpening;
+	private bool isFinished;
+	private bool doorIsMoving;
 	public AudioSource audioData;
 	public AudioSource audioData2;
+	private float buttonActivationPosition;
+	private float stopDoorPosition;
+	private float defaultDoorPosition;
 	
-	void Update(){
-		if(!isOpen){
-			if(!doorIsOpening && transform.position.z > 777.3200f){
-				audioData.Play();
-				audioData2.Play();
-				doorIsOpening = true;
+	public Material defaultMaterial;
+	public Material newMaterial;
+	
+	void Start(){
+		if(CompareTag("doorLab")){
+			buttonActivationPosition = 777.3200f;
+			defaultDoorPosition = door.transform.position.x;
+			stopDoorPosition = 437.58f;
+		}else if(CompareTag("doorOutside")){
+			buttonActivationPosition = 761.2600f;
+			stopDoorPosition = 12f;
+		}else if(CompareTag("doorOutsideCloser")){
+			buttonActivationPosition = 763.3900f;
+			defaultDoorPosition = 12f;
+			stopDoorPosition = 6.68347f;
+		}
+	}
+	
+	void Update(){		
+		if(!isFinished){
+			if(!doorIsMoving){
+				if(CompareTag("doorOutsideCloser")){
+					if(transform.position.z < buttonActivationPosition){
+						doorIsMoving = true;
+					}
+				} else{
+					if(transform.position.z > buttonActivationPosition){
+						doorIsMoving = true;
+					}
+				}
+				if(doorIsMoving){
+					audioData.Play();
+					audioData2.Play();
+					GetComponent<Renderer>().material = newMaterial;
+				}			
 			}
-			if(doorIsOpening){
-				door.transform.Translate(Vector3.forward*Time.deltaTime*3);
+			if(doorIsMoving){
+				if(CompareTag("doorLab")){
+					door.transform.Translate(Vector3.forward*Time.deltaTime*2);
+				} else if(CompareTag("doorOutside")){
+					door.transform.Translate(Vector3.up*Time.deltaTime*2);
+				} else if(CompareTag("doorOutsideCloser")){
+					door.transform.Translate(Vector3.down*Time.deltaTime*2);
+				}
 				
+				if(CompareTag("doorLab")){
+					if(door.transform.position.x >= stopDoorPosition){
+						isFinished = true;
+						doorIsMoving = false;
+					}
+				} else if(CompareTag("doorOutside")){
+					if(door.transform.position.y >= stopDoorPosition){
+						isFinished = true;
+						doorIsMoving = false;
+					}
+				} else if(CompareTag("doorOutsideCloser")){
+					if(door.transform.position.y <= stopDoorPosition){
+						isFinished = true;
+						doorIsMoving = false;
+					}
+				}
 			}
-			if(door.transform.position.x > 437.58f){
-				isOpen = true;
+		}else if(CompareTag("doorLab")){
+			if(!doorIsMoving){
+				if(transform.position.z > buttonActivationPosition){
+					doorIsMoving = true;
+					audioData.Play();
+					audioData2.Play();
+					GetComponent<Renderer>().material = defaultMaterial;
+				}
+			}
+			if(doorIsMoving){
+				door.transform.Translate(Vector3.back*Time.deltaTime*2);
+				if(door.transform.position.x <= defaultDoorPosition){
+					isFinished = false;
+					doorIsMoving = false;
+				}
+			}
+		}else if(CompareTag("doorOutsideCloser")){
+			if(!doorIsMoving){
+				if(transform.position.z < buttonActivationPosition){
+					doorIsMoving = true;
+					audioData.Play();
+					audioData2.Play();
+					GetComponent<Renderer>().material = defaultMaterial;
+				}
+			}
+			if(doorIsMoving){
+				door.transform.Translate(Vector3.up*Time.deltaTime*2);
+				if(door.transform.position.y >= defaultDoorPosition){
+					isFinished = false;
+					doorIsMoving = false;
+				}
 			}
 		}
 	}
---
+++ OculusQuestSurvivalOnMars/Assets/Scripts/plantAnalyser.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plantAnalyser : MonoBehaviour
+{
+    
+	private float defaultPositionY;
+	public Material defaultMaterial;
+	public Material newMaterial;
+	public AudioSource audioData;
+	public AudioSource audioData2;
+	public AudioSource audioData3;
+	
+	private float activationPosition = 5.462f;
+	private bool plantPositioned;
+	private int nrOfAttemptsToSucceed;
+	private int nrOfAttempts;
+	
+	private bool finished;
+	private bool boolAnalyzing;
+	
+	public GameObject noPlant;
+	public GameObject analyzing;
+	public GameObject toxic;
+	public GameObject edible;
+	
+    void Start(){
+		defaultPositionY = transform.position.y - 0.0001f;
+		nrOfAttemptsToSucceed = Random.Range(3,6);
+		Debug.Log(nrOfAttemptsToSucceed);
+	}
+	
+    void Update(){
+		if(!finished){
+			if(!plantPositioned && !boolAnalyzing){
+				if(transform.position.y <= activationPosition){
+					plantPositioned = true;
+					nrOfAttempts++;
+					audioData.Play();
+					GetComponent<Renderer>().material = newMaterial;
+					StartCoroutine(doAnalyzing());
+				}
+			}else{
+				if(transform.position.y >= defaultPositionY){
+					if(boolAnalyzing){
+						nrOfAttempts--;
+						analyzing.SetActive(false);
+					}
+					plantPositioned = false;
+					noPlant.SetActive(true);
+					toxic.SetActive(false);
+					edible.SetActive(false);
+					GetComponent<Renderer>().material = defaultMaterial;
+				}
+			}
+		}
+	}
+	
+	private IEnumerator doAnalyzing()
+    {
+        boolAnalyzing = true;
+		noPlant.SetActive(false);
+		analyzing.SetActive(true);
+		yield return new WaitForSeconds(8f);
+		analyzing.SetActive(false);
+		if(nrOfAttempts >= nrOfAttemptsToSucceed){
+			finished = true;
+			edible.SetActive(true);
+			audioData3.Play();
+		}else{
+			toxic.SetActive(true);
+			audioData2.Play();
+		}
+		boolAnalyzing = false;
+    }
+}
