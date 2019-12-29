20e408d99bf8ca327bba2be6aee89e4a48dbde96
legendaryXandi <45796104+legendaryXandi@users.noreply.github.com>
2019-12-29 21:06:29 +0100

    mainMenu

---
+++ OculusQuestSurvivalOnMars/Assets/Scripts/changeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class changeScene : MonoBehaviour
+{
+	public string sceneName;
+
+    public void changeToScene(){
+		SceneManager.LoadSceneAsync(sceneName);
+		GetComponentInChildren<Text>().text = "Loading...";
+	}
+}
