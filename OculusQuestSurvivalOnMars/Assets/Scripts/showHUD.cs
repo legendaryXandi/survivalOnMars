c3d8fcc892aafa318582089348b1b0d4e791c837
legendaryXandi <45796104+legendaryXandi@users.noreply.github.com>
2019-12-30 19:52:08 +0100

    improved HUD

---
+++ OculusQuestSurvivalOnMars/Assets/Scripts/showHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showHUD : MonoBehaviour
+{
+	
+	public GameObject canvas;
+	private GameObject oxygenSlider;
+	private GameObject radiationSlider;
+	
+	void Start(){
+		oxygenSlider = GameObject.FindGameObjectWithTag("oxygen");
+		radiationSlider = GameObject.FindGameObjectWithTag("radiation");
+	}
+	
+	void OnTriggerEnter(){
+		canvas.SetActive(true);
+		oxygenSlider.GetComponent<Slider>().SetValueWithoutNotify(100f);
+		radiationSlider.GetComponent<Slider>().SetValueWithoutNotify(0f);
+	}
+}
